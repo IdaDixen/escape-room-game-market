2fe327dc8537c7091d372d730ac4054d6a6a40eb
IdaDixen <idaoem@outlook.dk>
2026-07-24 16:34:30 +0200

    Added filtering function and renderGames function

--- script.js
+++ script.js
@@ -1,28 +1,77 @@
 
-function createGameElement(game) {
+function createGameAd(game) {
     let element = document.createElement("div");
     element.classList.add("game");
     element.innerHTML = `
         <h3>${game.title}</h3>
-        <p>Pris: ${game.price} kr.</p>
+        <p>Sprog: ${game.language}</p>
         <p>Stand: ${game.condition}</p>
         <p>Antal gange spillet: ${game.timesPlayed}</p>
+        <p class="price">${game.price} kr.</p>
     `;
     return element;
 }
 
+function renderGames(games) {
+    const gameAds = document.querySelector(".game-list");
+    gameAds.innerHTML = "";
+    games.forEach(game => {
+        let ad = createGameAd(game);
+        gameAds.appendChild(ad);
+    });
+}   
 
-const game = {
-    title: "Unlock 2",
-    price: 120,
-    condition: "Som ny",
-    timesPlayed: 5,
-    // description: "Et spændende escape room spil, der kan spilles hjemme. Indeholder flere scenarier og gåder.",
-    // imageUrl: "images/escape-room-the-game.jpg"
-};
+function filterGames(games) {
+/*     const languageFilter = document.getElementById("language-filter").value;
+    const conditionFilter = document.getElementById("condition-filter").value;
+    const priceFilter = document.getElementById("price-filter").value;
+ */
+    const languageFilter = null;//"Dansk";
+    const conditionFilter = null;
+    const priceFilter = null;//100;
 
-let ad = createGameElement(game);
-document.querySelector(".game-list").appendChild(ad);
+    const filteredGames = games.filter(game => languageFilter != null? game.language === languageFilter: true)
+                                .filter(game => conditionFilter != null? game.condition === conditionFilter: true)
+                                .filter(game => priceFilter != null? game.price <= priceFilter: true)
 
+    return filteredGames
+}
 
-console.log(ad);
+const games = [
+    {
+        title: "Unlock 2",
+        language: "Engelsk",
+        price: 120,
+        condition: "Som ny",
+        timesPlayed: 5
+        // imageUrl: "images/escape-room-the-game.jpg"
+    },
+    {
+        title: "Exit: The Game",
+        language: "Dansk",
+        price: 80,
+        condition: "God, men brugt",
+        timesPlayed: 1
+        // imageUrl: "images/exit-the-game.jpg"
+    },
+    {
+        title: "Escape Room: The Game 2",
+        language: "Dansk",
+        price: 150,
+        condition: "Uåbnet",
+        timesPlayed: 0
+        // imageUrl: "images/escape-room-the-game.jpg"
+    },
+    {
+        title: "Deckscape: Test Time",
+        language: "Engelsk",
+        price: 100,
+        condition: "God, men brugt",
+        timesPlayed: 3
+        // imageUrl: "images/deckscape-test-time.jpg"
+    }
+];
+
+const filteredGames = filterGames(games)
+
+renderGames(filteredGames);
